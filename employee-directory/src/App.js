4daa854558ded8fa75ac2b7ee1d9ebf25ca408a1
Casey Morgan <caseyjeanmorgan@gmail.com>
2020-11-17 17:57:45 -0600

    waiting on further instruction to implement api key provided. Played with a few versions in List.js to implement a mapped array located in Apps.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -3,6 +3,23 @@ import Jumbotron from "./components/Jumbotron/jumbotron"
 import Input from "./components/Input/Input"
 import List from "./components/List/List"
 
+const ED = [
+  {
+    id: 1,
+    name: "Casey Morgan",
+    phone: "715-651-4130",
+    email: "caseyjeanmorgan@gmail.com",
+    DOB: "02-23-1989"
+  },
+  {
+    id: 2,
+    name: "Brad Foytik",
+    phone: "715-458-6221",
+    email: "brad.foytik@gmail.com",
+    DOB: "09-9-1992"
+  }
+
+]
 
 function App() {
   return (
@@ -10,7 +27,7 @@ function App() {
       <Jumbotron />
       <Input />
       <br></br>
-      <List />
+      <List ED={ED} />
     </div>
   );
 }
--- employee-directory/src/components/List/List.js
+++ employee-directory/src/components/List/List.js
@@ -2,18 +2,60 @@ import React from "react";
 import "./List.css";
 
 
-
-function List() {
+function List(props) {
 
   return (
-    <ul class="list-group">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
+    <ul className="list-group">
+      {props.ED.map(item => (
+        <li className="list-group-item" key={item.id}>
+          {item.name} {""}
+          {item.phone} {""}
+          {item.email} {""}
+          {item.DOB} {""}
+        </li>
+      ))}
+    </ul>
+    // <ul className="list-group">
+    //   <div class= "row">
+    //       <div class= "col">
+    //         <li className="list-group-item">Image</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">Name</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">Phone</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">Email</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">DOB</li>
+    //       </div>
+    //     </div>
+    //   {props.ED.map(item => (
+    //     <div class= "row">
+    //       <div class= "col">
+    //         <li className="list-group-item" key={item.id}></li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">{item.name}</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">{item.phone}</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">{item.email}</li>
+    //       </div>
+    //       <div class= "col">
+    //         <li class="list-group-item">{item.DOB}</li>
+    //       </div>
+    //     </div>
+    //   ))}
+    // </ul>
+    
   );
 }
 
 export default List;
+
